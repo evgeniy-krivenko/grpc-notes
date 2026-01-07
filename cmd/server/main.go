cd44ef24b1c8960f8931c3f542a974415894cd3b
Evgeniy Krivenko <evgeniy.krivenko.work@gmail.con>
2026-01-07 23:51:30 +0300

    feat: impl websocket, swagger server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,24 +5,33 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"buf.build/go/protovalidate"
 	protovalidateic "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/protovalidate"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/rs/cors"
+	"github.com/tmc/grpc-websocket-proxy/wsproxy"
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/keepalive"
 
+	openapi "github.com/evgeniy-krivenko/grpc-notes/docs/api/notes/v1"
 	notesapi "github.com/evgeniy-krivenko/grpc-notes/internal/api/notes"
 	"github.com/evgeniy-krivenko/grpc-notes/internal/config"
 	"github.com/evgeniy-krivenko/grpc-notes/internal/ctxtr"
 	"github.com/evgeniy-krivenko/grpc-notes/internal/repository"
 	notesusecase "github.com/evgeniy-krivenko/grpc-notes/internal/usecase/notes"
+	gw "github.com/evgeniy-krivenko/grpc-notes/pkg/api/notes/v1"
 	"github.com/evgeniy-krivenko/grpc-notes/pkg/database"
 	"github.com/evgeniy-krivenko/grpc-notes/pkg/grpcx"
+	"github.com/evgeniy-krivenko/grpc-notes/pkg/gwserver"
 	"github.com/evgeniy-krivenko/grpc-notes/pkg/logger/slogx"
+	"github.com/evgeniy-krivenko/grpc-notes/third_party/swagger"
 )
 
 func main() {
@@ -80,6 +89,16 @@ func run() error {
 		return fmt.Errorf("init notes api: %v", err)
 	}
 
+	gwSrv, err := buildGWServer(ctx, &cfg)
+	if err != nil {
+		return fmt.Errorf("build gateway server: %v", err)
+	}
+
+	swaggerSrv, err := buildSwaggerServer(&cfg)
+	if err != nil {
+		return fmt.Errorf("build swagger server: %v", err)
+	}
+
 	srv, err := grpcx.New(grpcx.NewOptions(
 		cfg.GRPC.Addr,
 		grpcx.WithLogger(logger),
@@ -108,10 +127,58 @@ func run() error {
 	eg, ctx := errgroup.WithContext(ctx)
 
 	eg.Go(func() error { return srv.Run(ctx) })
+	eg.Go(func() error { return gwSrv.Run(ctx) })
+	eg.Go(func() error { return swaggerSrv.Run(ctx) })
 
 	if err := eg.Wait(); err != nil && !errors.Is(err, context.Canceled) {
 		return fmt.Errorf("wait app stop: %v", err)
 	}
 
 	return nil
+}
+
+func buildGWServer(ctx context.Context, cfg *config.Config) (*gwserver.Server, error) {
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+
+	if err := gw.RegisterNoteAPIHandlerFromEndpoint(ctx, mux, cfg.GRPC.Addr, opts); err != nil {
+		return nil, fmt.Errorf("register grpc gateway: %v", err)
+	}
+
+	corsMiddleware := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodDelete,
+		},
+	})
+
+	wsMiddleware := func(h http.Handler) http.Handler {
+		return wsproxy.WebsocketProxy(h)
+	}
+
+	return gwserver.New(gwserver.NewOptions(
+		cfg.HTTP.Addr,
+		mux,
+		gwserver.WithMiddlewares(corsMiddleware.Handler, wsMiddleware),
+		gwserver.WithLogger(slogx.Default()),
+	))
+}
+
+func buildSwaggerServer(cfg *config.Config) (*gwserver.Server, error) {
+	mux := http.NewServeMux()
+
+	swaggerStaticsHandler := http.StripPrefix("/swagger", http.FileServer(http.FS(swagger.Content)))
+	mux.Handle("GET /swagger/", swaggerStaticsHandler)
+
+	swaggerSpecsHandler := http.StripPrefix("/swagger/specs", http.FileServer(http.FS(openapi.Content)))
+	mux.Handle("GET /swagger/specs/", swaggerSpecsHandler)
+
+	return gwserver.New(gwserver.NewOptions(
+		cfg.SwaggerHTTP.Addr,
+		mux,
+		gwserver.WithLogger(slogx.Default()),
+	))
 }
